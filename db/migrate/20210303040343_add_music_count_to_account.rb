d95d993eef445984169ce5fc71f12a88d7c70574
lljxx1 <lljxx1@gmail.com>
2021-03-09 10:19:26 +0800

    add

--- db/migrate/20210303040343_add_music_count_to_account.rb
+++ db/migrate/20210303040343_add_music_count_to_account.rb
@@ -1,9 +1,7 @@
 class AddMusicCountToAccount < ActiveRecord::Migration[5.2]
   def change
     add_column :account_stats, :musics_count, :bigint
-    safety_assured do
-      change_column_default :account_stats, :musics_count, 0
-    end
+    change_column :account_stats, :musics_count, :bigint, default: 0
   end
   def down
     remove_column :account_stats, :musics_count
