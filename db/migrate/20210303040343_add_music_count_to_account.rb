a823d937f5fdf1d26d955c04a502500182519ea9
fun <lljxx1@gmail.com>
2021-03-09 10:05:39 +0800

    add safe

--- db/migrate/20210303040343_add_music_count_to_account.rb
+++ db/migrate/20210303040343_add_music_count_to_account.rb
@@ -1,7 +1,9 @@
 class AddMusicCountToAccount < ActiveRecord::Migration[5.2]
   def change
     add_column :account_stats, :musics_count, :bigint
-    change_column_default :account_stats, :musics_count, 0
+    safety_assured do
+      change_column_default :account_stats, :musics_count, 0
+    end
   end
   def down
     remove_column :account_stats, :musics_count
